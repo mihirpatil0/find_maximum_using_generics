2f7bcca15de94da7df91d40f577b63ad1ab91c34
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-30 13:18:39 +0530

    [mihir] Added: found maximum float number using compareTo() method.

--- src/com/bridgelab/maximumusinggenerics/FindMaximum.java
+++ src/com/bridgelab/maximumusinggenerics/FindMaximum.java
@@ -4,20 +4,21 @@ package com.bridgelab.maximumusinggenerics;
  * @author mihir
  * @since 30-June-2021
  * 
- * Finding maximum problem using generic.
+ * Finding maximum numbers using generic.
  ****************************************/
 
 public class FindMaximum
 {
 	static Integer max;
+	static Float maxFloat;
 	/**
 	 * Name : maximumInteger
 	 * 
 	 * Description : Finding maximum number.
 	 * 
-	 * @param a
-	 * @param b
-	 * @param c
+	 * @param intNumeOne
+	 * @param intNumTwo
+	 * @param intNumThree
 	 * @return
 	 * 
 	 * Algorithm : Finding max integer using compareTo() method.
@@ -39,6 +40,34 @@ public class FindMaximum
 	}
 	
 	/**
+	 * Name : maximumFloat
+	 * 
+	 * Description : Finding maximum float number.
+	 * 
+	 * @param floatNumOne
+	 * @param floatNumTwo
+	 * @param floatNumThree
+	 * @return
+	 * Algorithm : Finding max Float value using compareTo() method.
+	 * 
+	 * Modification : First commit 30-June-2021
+	 */
+	public static Float maximumFloat(Float floatNumOne, Float floatNumTwo, Float floatNumThree)
+	{
+		maxFloat = floatNumOne;
+		
+		if(floatNumTwo.compareTo(maxFloat) > 0)
+		{
+			maxFloat = floatNumTwo;
+		}
+		if(floatNumThree.compareTo(maxFloat) > 0)
+		{
+			maxFloat = floatNumThree;
+		}
+		return maxFloat;
+	}
+	
+	/**
 	 * @param args
 	 * Passing integer value to function.
 	 * Calling maximumInteger function.
@@ -47,8 +76,12 @@ public class FindMaximum
 	public static void main(String[] args)
 	{
 		Integer intNumeOne = 15, intNumTwo = 55, intNumThree = 24;
+		Float floatNumOne = 218.13f, floatNumTwo = 555.453f, floatNumThree = 999.999f;
 		
 		maximumInteger(intNumeOne, intNumTwo, intNumThree);
 		System.out.println("Maximum amoungst : " + intNumeOne + ", " + intNumTwo + ", " + intNumThree + " : is : " + max);
+		
+		maximumFloat(floatNumOne, floatNumTwo, floatNumThree);
+		System.out.println("Maximum amoungst : " + floatNumOne + ", " + floatNumTwo + ", " + floatNumThree + " is : " + maxFloat);
 	}
 }
