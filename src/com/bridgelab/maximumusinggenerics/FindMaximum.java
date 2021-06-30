5a6929598242f7e969b37ed0274d832da9034d27
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-30 13:35:11 +0530

    [mihir] Added: finding maximum string from given strings using compareTo() method.

--- src/com/bridgelab/maximumusinggenerics/FindMaximum.java
+++ src/com/bridgelab/maximumusinggenerics/FindMaximum.java
@@ -11,6 +11,8 @@ public class FindMaximum
 {
 	static Integer max;
 	static Float maxFloat;
+	static String maxString;
+	
 	/**
 	 * Name : maximumInteger
 	 * 
@@ -68,6 +70,34 @@ public class FindMaximum
 	}
 	
 	/**
+	 * Name : maximumString
+	 * 
+	 * Description : Printing maximum String.
+	 * @param stringOne
+	 * @param stringTwo
+	 * @param stringThree
+	 * @return
+	 * 
+	 * Algorithm : Finding max String value using compareTo() method.
+	 * 
+	 * Modification : First commit 30-June-2021
+	 */
+	public static String maximumString(String stringOne, String stringTwo, String stringThree)
+	{
+		maxString = stringOne;
+		
+		if(stringTwo.compareTo(maxString) > 0)
+		{
+			maxString = stringTwo;
+		}
+		if(stringThree.compareTo(maxString) > 0)
+		{
+			maxString = stringThree;
+		}
+		return maxString;
+	}
+	
+	/**
 	 * @param args
 	 * Passing integer value to function.
 	 * Calling maximumInteger function.
@@ -77,11 +107,15 @@ public class FindMaximum
 	{
 		Integer intNumeOne = 15, intNumTwo = 55, intNumThree = 24;
 		Float floatNumOne = 218.13f, floatNumTwo = 555.453f, floatNumThree = 999.999f;
+		String stringOne = "Apple", stringTwo = "Peach", stringThree = "Banana";
 		
 		maximumInteger(intNumeOne, intNumTwo, intNumThree);
 		System.out.println("Maximum amoungst : " + intNumeOne + ", " + intNumTwo + ", " + intNumThree + " : is : " + max);
 		
 		maximumFloat(floatNumOne, floatNumTwo, floatNumThree);
 		System.out.println("Maximum amoungst : " + floatNumOne + ", " + floatNumTwo + ", " + floatNumThree + " is : " + maxFloat);
+		
+		maximumString(stringOne, stringTwo, stringThree);
+		System.out.println("Maximum amoungst : " + stringOne + ", " + stringTwo + ", " + stringThree + " is : " + maxString);
 	}
 }
